56fa593d282d3d4d8ce24a28e573bd041dbd4ed0
jrmyklyn <jeremyklyn@gmail.com>
2023-11-06 22:03:41 -0600

    cleanup

--- keyboards/royalkludge/rk100/rgb/rgb.c
+++ keyboards/royalkludge/rk100/rgb/rgb.c
@@ -1,7 +1,7 @@
 #include "quantum.h"
 #include "rgb_matrix.h"
 
-#define LED5_RBG_MATRIX_INDEX 92
+#define LED5_RGB_MATRIX_INDEX 92
 #define LED234_RGB_MATRIX_INDEX 93
 
 bool rgb_matrix_indicators_kb(void) {
@@ -9,7 +9,7 @@ bool rgb_matrix_indicators_kb(void) {
         return false;
     }
     led_t current_host_keyboard_led_state = host_keyboard_led_state();
-    rgb_matrix_set_color(LED5_RBG_MATRIX_INDEX, 0, current_host_keyboard_led_state.num_lock ? 255 : 0, 0);
+    rgb_matrix_set_color(LED5_RGB_MATRIX_INDEX, 0, current_host_keyboard_led_state.num_lock ? 255 : 0, 0);
     rgb_matrix_set_color(LED234_RGB_MATRIX_INDEX, 0, current_host_keyboard_led_state.caps_lock ? 255 : 0, 255);
     return true;
 }
