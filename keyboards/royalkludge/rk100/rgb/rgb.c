90b0ff4364a4b6039af5afe7b2c9d8d9c61f3f85
jrmyklyn <jeremyklyn@gmail.com>
2023-11-08 01:46:46 -0600

    fixing rgb problems wip...

--- keyboards/royalkludge/rk100/rgb/rgb.c
+++ keyboards/royalkludge/rk100/rgb/rgb.c
@@ -9,7 +9,9 @@ bool rgb_matrix_indicators_kb(void) {
         return false;
     }
     led_t current_host_keyboard_led_state = host_keyboard_led_state();
-    rgb_matrix_set_color(LED5_RGB_MATRIX_INDEX, 0, current_host_keyboard_led_state.num_lock ? 255 : 0, 0);
-    rgb_matrix_set_color(LED234_RGB_MATRIX_INDEX, 0, current_host_keyboard_led_state.caps_lock ? 255 : 0, 255);
+    rgb_matrix_set_color(LED5_RGB_MATRIX_INDEX, 0, (current_host_keyboard_led_state.num_lock ? 255 : 0), 0);
+    rgb_matrix_set_color(LED234_RGB_MATRIX_INDEX, 0, (current_host_keyboard_led_state.caps_lock ? 255 : 0), 255);
+    rgb_matrix_set_color(0, 63, 63, 63); // TODO why does green always break on a row if set in this function?
+                                        // and why does it beak on one row, but not other rows???
     return true;
 }
